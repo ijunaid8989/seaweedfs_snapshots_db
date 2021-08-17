20f353d4a56c3aef35d82a7b4beb658ee766d34a
Junaid Farooq <junaid@evercam.io>
2021-08-17 23:13:21 +0500

    Start Kafka on start up.

--- lib/seaweedfs_snapshots_db/application.ex
+++ lib/seaweedfs_snapshots_db/application.ex
@@ -8,7 +8,7 @@ defmodule SeaweedfsSnapshotsDb.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # {SeaweedfsSnapshotsDb, []}
+      {SeaweedfsSnapshotsDb, []}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
